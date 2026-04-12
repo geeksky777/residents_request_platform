5c4f68ef2a893f6c4f15936ac255a02bd9513fc2
Whytee <elatfullin@gmail.com>
2026-04-12 20:57:36 +0300

    fix local host

--- management-service/app/config.py
+++ management-service/app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = (
         "postgresql+asyncpg://management_user:management_pass@localhost:5433/management_db"
     )
-    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
+    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9094"
 
 
 settings = Settings()
--- request-service/app/config.py
+++ request-service/app/config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     DATABASE_URL: str = (
         "postgresql+asyncpg://request_user:request_pass@localhost:5432/request_db"
     )
-    KAFKA_BOOTSTRAP_SERVERS: str = "kafka:9092"
+    KAFKA_BOOTSTRAP_SERVERS: str = "localhost:9094"
 
 
 settings = Settings()
